5746290dc4bbdb7730e68c503907fe542a5b25ee
Muzaffer Yağız Yasak <yagiz.ysk@hotmail.com>
2020-10-01 08:00:58 +0300

    Update hashturner.py

--- hashturner.py
+++ hashturner.py
@@ -16,8 +16,7 @@ def hashturner():
     hashlib_kelime = hashlib.md5(kelime.encode()).hexdigest()
     global x
     x = hashlib_kelime
-    kelime_girisi.insert(0, x)
-
+    yazi_3.config(text=x)
 def copy_hash():
     root.clipboard_append(x)
 baslik = tk.Label(root, text="HashTurner", font=('Helvetica',12,'bold'))
@@ -25,6 +24,10 @@ baslik.place(x=210,y=10)
 
 yazi = tk.Label(root, text="İstediğiniz Kelimeyi Girin:", font=('verdana', 10, 'bold'))
 yazi.place(x=8, y=65)
+yazi_2 = tk.Label(root, text="Md5:", font=('verdana', 10, 'bold'))
+yazi_2.place(x=8, y=110)
+yazi_3 = tk.Label(root, text="", font=('verdana', 10))
+yazi_3.place(x=60, y=110)
 kelime_girisi = tk.Entry(root)
 kelime_girisi.place(x=199, y=65)
 
